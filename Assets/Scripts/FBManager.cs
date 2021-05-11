1707ad19ee31719c27b3df015a943ba9a003dbfc
Lee Jae Hyun <myleje@gmail.com>
2021-05-11 11:32:35 +0900

    .

--- Assets/Scripts/FBManager.cs
+++ Assets/Scripts/FBManager.cs
@@ -66,4 +66,41 @@ public class FBManager : MonoBehaviour
         // UserData 노드를 생성하고 하위에 데이터를 추가
         reference.Child("UserData").Child(_userName).SetRawJsonValueAsync(json);
     }
+
+    /*
+        데이터 조회
+        - GetValueAsync 사용
+        - 비동기 방식, 호출완료된 후 데이터 가공
+    */
+
+    public void LoadAllData()
+    {
+        // 지역 레퍼런스 선언
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.GetReference("UserData");
+        // 데이터 조회
+        reference.GetValueAsync().ContinueWith(task => {
+            if (task.IsFaulted)
+            {
+                Debug.LogError("Failed load data !!!");
+            }
+            else if (task.IsCompleted)
+            {
+                // 스냅샷 생성 : 조회한 데이터(레코드)를 저장하는 단위
+                DataSnapshot snapshot = task.Result;
+
+                // 데이터 건수 출력
+                Debug.Log("데이터 레코드 갯수" + snapshot.ChildrenCount);
+
+                // 데이터 출력
+                foreach (DataSnapshot data in snapshot.Children)
+                {
+                    // DataSnapshot.Value 데이터를 접근
+                    // {키}:{값} IDictionary 자료형 사용 (Key:Value)
+                    IDictionary _data = (IDictionary) data.Value;
+                    Debug.Log($"");
+                }
+            }
+        });
+    }
+
 }
